7c5a04b15bb404698066f35ef73cd99fe0136981
HATTORIULTRA <throwbvck@gmail.com>
2026-01-13 04:20:20 +0500

    07: projects

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,7 @@
-"use client";
+import { ProjectsView } from "@/features/projects/components/projects-view";
 
-import { useMutation, useQuery } from "convex/react";
-import { api } from "../../convex/_generated/api";
-import { Button } from "@/components/ui/button";
-
-function X() {
-  const projects = useQuery(api.projects.get);
-  const createProject = useMutation(api.projects.create);
-  return (
-    <>
-      <Button onClick={() => createProject({ name: "new project123123" })}>
-        Add new
-      </Button>
-      <ul className="flex flex-col gap-2 p-4">
-        {projects?.map((project) => (
-          <li className="border rounded p-2 flex flex-col" key={project._id}>
-            <p>{project.name}</p>
-            <p>Owner ID: {`${project.ownerId}`}</p>
-          </li>
-        ))}
-      </ul>
-    </>
-  );
+function Home() {
+  return <ProjectsView />;
 }
 
-export default X;
+export default Home;
